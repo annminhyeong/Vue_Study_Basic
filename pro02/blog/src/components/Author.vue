636769b30bd06011d002ee87eb482d34191f2b32
minh <rnrnck78@naver.com>
2022-05-23 12:25:13 +0900

    pro02 complete

---
+++ pro02/blog/src/components/Author.vue
@@ -0,0 +1,11 @@
+<template>
+  <h5>작가소개입니다.</h5>
+</template>
+
+<script>
+export default {
+  name: 'Author',
+};
+</script>
+
+<style></style>
---
+++ pro02/blog/src/components/List.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-for="(data, i) in 블로그글" :key="data">
+    <h5 @click="$router.push(`/detail/${i}`)">{{ data.title }}</h5>
+    <p>{{ data.date }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  props: {
+    블로그글: Array,
+  },
+};
+</script>
+
+<style></style>
